5e9b7e0d5b1876aca04e51d0d2dc7d3be1f9f513
Steve Koenig <swk7@yahoo.com>
2018-12-03 17:16:23 -0500

    more maps

--- src/app/pages/map/map.component.ts
+++ src/app/pages/map/map.component.ts
@@ -7,13 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapComponent implements OnInit {
 
+    eightZeros = [0,0,0,0,0,0,0,0];
+    sevenZeros = [0,0,0,0,0,0,0];
+
     theMap = [
-        [191,191,191,169,0,0,0,0,0,0,0,0,180,191,191,170,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-        [191,191,191,175,178,0,0,0,181,182,184,183,182,179.187,176,0,0,0,0,0,0,0,0,0,0,0,0,0,],
-        [191,191,191,191,175,184,183,185,191,191,182,184,183,182,179.187,176,0,0,0,0,0,0,0,0,0,0,0,0,0,]
+        [191,191,191,169,0,0,0,0,0,0,0,0,180,191,191,170].concat(this.sevenZeros).concat(this.eightZeros).concat(this.eightZeros).concat(this.sevenZeros),
+        [191,191,191,175,178,0,0,0,181,182,184,183,182,179,187,176].concat(this.sevenZeros).concat(this.eightZeros).concat(this.eightZeros).concat(this.sevenZeros),
+        [191,191,191,191,175,184,183,185,191,191,177,176,71,157,155].concat(this.eightZeros).concat(this.eightZeros).concat(this.eightZeros).concat(this.sevenZeros),
     ]
 
-    constructor() { }
+    constructor() {
+        console.log(this.theMap[0].length);
+        console.log(this.theMap[1].length);
+        console.log(this.theMap[2].length);
+     }
 
     ngOnInit() {
     }
